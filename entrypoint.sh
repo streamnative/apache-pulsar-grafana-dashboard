7c658ffbc651b4501b5183ceb717424ddb89d69b
冉小龙 <rxl@apache.org>
2020-05-09 13:05:57 +0800

    Replace loki datasource (#42)
    Signed-off-by: xiaolong.ran <rxl@apache.org>
    
    - Replace loki datasource

--- entrypoint.sh
+++ entrypoint.sh
@@ -35,7 +35,7 @@ chmod 0400 /var/lib/grafana/grafana.ini
 # apply environment variables to pulsar provisioned dashboards
 if [ "x${GRAFANA_USE_BUILTIN_DASHBOARDS}" == "xtrue" ]; then
     for item in `ls /var/lib/grafana/pulsar_provisioning/dashboard_templates`; do
-      sed -e 's/{{ PULSAR_CLUSTER }}/${PULSAR_CLUSTER}/' -e 's/{{ GF_LOKI_DATASOURCE_NAME }}/${GF_LOKI_DATASOURCE_NAME}/' /var/lib/grafana/pulsar_provisioning/dashboard_templates/${item} > /var/lib/grafana/pulsar_provisioning/dashboards/${item}
+      sed "s/{{ PULSAR_CLUSTER }}/${PULSAR_CLUSTER}/; s/{{ GF_LOKI_DATASOURCE_NAME }}/${GF_LOKI_DATASOURCE_NAME}/" /var/lib/grafana/pulsar_provisioning/dashboard_templates/${item} > /var/lib/grafana/pulsar_provisioning/dashboards/${item}
     done
 fi
 
